a59397116a5b1444537d63e34b31633f01789a80
bencomo911 <bencomo911@gmail.com>
2023-02-19 16:04:30 -0800

    Section 2- Customize the Text View Added some notes

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -7,18 +7,23 @@
 
 import SwiftUI
 
+// By default, Swift declares two structures:
+// First structure conforms to View structure and describes the view's content and layout
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Mundo!")
+            Text("Turtle Rock")
+                .font(.title)
+                
         }
         .padding()
     }
 }
 
+// Second structure declares a preview for that View
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
