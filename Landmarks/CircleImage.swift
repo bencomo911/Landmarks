820c2268e4002e9bc18d1ea37ce627b73e361ea5
bencomo911 <bencomo911@gmail.com>
2023-03-01 14:34:07 -0800

    Section 4 - Create a Custom Image View Added landmark image

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Dafne Bencomo on 3/1/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 5)
+            }
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
